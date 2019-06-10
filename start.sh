b92ce9ae5f0018674a3e52ff78efc7f3863d3253
Brazilian unknown <teclucasroot@gmail.com>
2019-06-10 10:26:00 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+$(id -u) -ne 0 ] && { echo -e "root?"; sudo mitm.sh ; exit ;} || echo -e "Ok"
+apt install dialog
 dialog                                         \
    --title 'ALUNOS'                           \
    --infobox '\nAndré    - 01\nBruna    - 05\nFernando - 19\nLucas    - 27\nMarcelo  - 28\nMatheus  - 53\nMelissa  - 32\nRafaela  - 37\nSimão    - 44\nThallya  - 47\nWellison - 49' \
